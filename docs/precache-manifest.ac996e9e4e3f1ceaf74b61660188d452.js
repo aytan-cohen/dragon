f4c46cf7cd8bf7c991bfbe335d1a9c4a67e3aaa9
Jewdeer21 <ayecohen21@gmail.com>
2019-06-13 18:13:28 -0400

    created todo app

---
+++ docs/precache-manifest.ac996e9e4e3f1ceaf74b61660188d452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27e413d3ea2f5a9a40fc1865a830947",
+    "url": "/dragon/index.html"
+  },
+  {
+    "revision": "a8a92f03d31c2bf34797",
+    "url": "/dragon/static/css/main.5bcddd9b.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/dragon/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "a8a92f03d31c2bf34797",
+    "url": "/dragon/static/js/main.92201c27.chunk.js"
+  },
+  {
+    "revision": "69ef154f655dbaf5323f",
+    "url": "/dragon/static/js/runtime~main.f1e359e7.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: "",
+      items: [
+        { id: 1, text: "Learn React", completed: false },
+        { id: 2, text: "Build a todo app", completed: false },
+        { id: 3, text: "Profit", completed: false }
+      ]
+    }
+  }
+
+  addToList = (e) => {
+    e.preventDefault();
+    if(this.state.item.length < 1) 
+      return
+    this.setState({
+        items: [...this.state.items, this.state.item],
+        item: ""
+    }, () => {
+      console.log(this.state.items);
+    })   
+  }
+
+  itemUpdate = (e) => {
+    this.setState({item: e.target.value});
+  }
+
+  gotIt = (i) => {
+    console.log(i);
+    let items = [...this.state.items];
+    items[i].completed = !items[i].completed;
+    this.setState([items, i]);
+  }
+
+  render(){
+    return (
+      <div className="Dragon">
+        <div className="jumbotron">
+        <h1>Dojo To Do List</h1>
+        </div>
+        <br /><hr /><br />
+        <form onSubmit={this.addToList}>
+          <div className="form-group">
+            <label>To Do List:
+            <input 
+              className="form-control" 
+              type="text" 
+              name="item"
+              value={this.state.item}
+              onChange={this.itemUpdate}
+              />
+            </label>
+          </div>
+          <input type="submit" className="btn btn-primary" />
+        </form>
+        <br /><hr /><br />
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Completed?</th>
+              <th>Item</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.items.map( (item, i) =>
+              <tr key={i}>
+                <td onClick={this.gotIt.bind(this, i)}>
+                {
+                  item.completed ?
+                  <>
+                  <input type="checkbox" defaultChecked /> &nbsp;
+                  <span className="badge badge-success">Finished!</span> 
+                  </>:
+                  <>
+                  <input type="checkbox" /> &nbsp;
+                  <span className="badge badge-danger">Completed???</span>
+                  </>
+                }
+                </td>
+                <td>{item.text}</td>
+              </tr>
+              )
+            }
+          </tbody>
+
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
